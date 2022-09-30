7353cfadb3b677e16fd94b59852ed22268140cb1
剑花烟雨江南 <15986616910@163.com>
2022-09-30 11:38:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6vp-6ahSXcyvVMbixNLfl-MUN6o',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '6cMGcRYjgKSNbu1_EkSmbUpUpKh7dhMhWKqHGSucl4M',
       // 所在省份
       province: '上海',
       // 所在城市
