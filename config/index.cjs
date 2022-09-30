615a0cb228f0dbb8de48670c3a934dcd90a9465d
剑花烟雨江南 <15986616910@163.com>
2022-09-30 11:56:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,7 +12,7 @@ const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: 'e24eaabf958db1e0893acd323229313d',
+  APP_SECRET: '303a1180af70bbb25a259568640d1e28',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
